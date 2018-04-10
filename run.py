0d25062322302f101b4c726579e51a85aa156a58
jeremy <305972991@qq.com>
2018-04-10 21:44:14 +0800

    更改目标时间点变量命名

--- run.py
+++ run.py
@@ -4,11 +4,12 @@ import time
 
 # 实时监控
 while True:
+    # 当前时间戳
     now_tamp = int(time.time())
     # 指定需要监控的时间点
-    now_time = "2018-04-10 21:31:00"
+    target_time = "2018-04-10 21:31:00"
     # 转换成时间数组
-    time_array = time.strptime(now_time,"%Y-%m-%d %H:%M:%S")
+    time_array = time.strptime(target_time,"%Y-%m-%d %H:%M:%S")
     # 转换成时间戳
     times_tamp = int(time.mktime(time_array))
     # 判断当前时间是否已经到了指定时间点
